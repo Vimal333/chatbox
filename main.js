a08340569058688dd48487f3dda6ac1858256d5a
Vimal333 <33688435+Vimal333@users.noreply.github.com>
2017-12-15 18:12:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function getwords1() {
+  text1 = words1.value;
+  document.getElementById("meg1").innerHTML += '<p>'+text1
+  document.getElementById("words1").value = "";
+}
+
+function getwords2() {
+  text2 = words2.value;
+  document.getElementById("meg2").innerHTML += '<p>'+text2
+  document.getElementById("words2").value = "";
+}
